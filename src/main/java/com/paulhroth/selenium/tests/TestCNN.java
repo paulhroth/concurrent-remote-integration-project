5f94e2afe970f1bdf0cbc34c272e00d2fdad293f
Paul <paul.roth@polarmobile.com>
2013-06-20 23:16:26 -0400

    Included example of javascript execution in TestCNN.java, and updated TestParallel.java to use baseurls.properties

--- src/main/java/com/paulhroth/selenium/tests/TestCNN.java
+++ src/main/java/com/paulhroth/selenium/tests/TestCNN.java
@@ -11,9 +11,11 @@ import com.saucelabs.junit.SauceOnDemandTestWatcher;
 import com.saucelabs.junit.Parallelized;
 import com.saucelabs.saucerest.SauceREST;
 
+import java.io.FileInputStream;
 import java.net.URL;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Properties;
 import java.util.Random;
 
 import org.junit.After;
@@ -22,6 +24,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.CapabilityType;
@@ -32,15 +35,12 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 @RunWith(Parallelized.class)
-public class TestParallel {
+public class TestCNN {
 
     // setup strings: feel free to modify these as required
     private final String sauce_user = "polarqa";
-    private final String sauce_pass = "4testing";
     private final String sauce_access = "d609b648-22e3-44bb-a38e-c28931df837d";
-    private final String baseUrl = "Interpreter.getLink("src/main/config/baseurls.properties", "CNN");
-    private final String initialOwnerGroup = "AcmePublisher";
-    private final String finalOwnerGroup = "AcmeAdvertising";
+    private final String baseUrl = Interpreter.getLink("src/main/config/baseurls.properties", "CNN");
 
     // these are required for the test to work
     private WebDriver driver;
@@ -51,20 +51,7 @@ public class TestParallel {
     private String os;
     private String version;
 
-    // generates a random string in order to allow everything created by this
-    // test to be unique
-    public String generateString() {
-        StringBuilder sb = new StringBuilder();
-        Random random = new Random();
-        char[] chars = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-        for (int i = 0; i < 20; i++) {
-            char c = chars[random.nextInt(chars.length)];
-            sb.append(c);
-        }
-        return (sb.toString());
-    }
-
-    public TestParallel(String os, String version, String browser) {
+    public TestCNN(String os, String version, String browser) {
         super();
         this.os = os;
         this.version = version;
@@ -98,47 +85,19 @@ public class TestParallel {
     @Test
     public void webDriver() throws Exception {
         // loads site
-        driver.get(baseUrl + "/login");
-
-        // logs in
-        driver.findElement(By.id("id_username")).clear();
-        driver.findElement(By.id("id_username")).sendKeys(sauce_user);
-        driver.findElement(By.id("id_password")).clear();
-        driver.findElement(By.id("id_password")).sendKeys(sauce_pass);
-        driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
-
-        // navigates to the Native Ad Creator
-        driver.findElement(By.linkText("Native Ad Creator")).click();
-
-        // creates a New Group with the name
-        // "test_<random 20 character long string>"
-        final String groupName = generateString();
-        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver d) {
-                return d.findElements(By.linkText("+ New Group")).size() > 0;
-            }
-        });
-        driver.findElement(By.linkText("+ New Group")).click();
-        driver.findElement(By.id("id_name")).clear();
-        driver.findElement(By.id("id_name")).sendKeys("test_" + groupName);
-        new Select(driver.findElement(By.id("id_ownergroup")))
-                .selectByVisibleText(initialOwnerGroup);
-        driver.findElement(By.cssSelector("input.btn.btn-success")).click();
-
-        // modifies the newly created group
-        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver d) {
-                return d.findElements(
-                        By.xpath(".//*[@id='main']/div/table/tbody/tr/td/h2[contains(text(),'"
-                                + groupName + "')]/a")).size() > 0;
-            }
-        });
-        driver.findElement(
-                By.xpath(".//*[@id='main']/div/table/tbody/tr/td/h2[contains(text(),'"
-                        + groupName + "')]/a")).click();
-        new Select(driver.findElement(By.id("id_ownergroup")))
-                .selectByVisibleText(finalOwnerGroup);
-        driver.findElement(By.cssSelector("input.btn.btn-success")).click();
+        driver.get(baseUrl);
+        
+        
+        //injects native ads
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("var d=document;var s=d.createElement('script');s.setAttribute('type','text/javascript');s.setAttribute('charset','UTF-8');s.setAttribute('src','http://localhost:9999/bookmarklet/demo.js');d.body.appendChild(s);");
+        
+        //scrolls down to make ads visible in screenshot later
+        js.executeScript("window.scrollBy(0,450)");
+        
+        //asserts that the ads are displayed on the page
+        synchronized (driver) { driver.wait(10000); }
+        assertEquals("There should be 2 sponsored links on the page containing the link 'sponsored.html#!/'." , 2, driver.findElements(By.cssSelector("a[href*=\"sponsored.html#!/\"]")).size());   
 
         // passes the test
         passed = true;
